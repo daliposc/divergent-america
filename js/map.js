75955ef686dd19a3c27ac4141282432d485640ae
Connor D <daliposc@oregonstate.edu>
2017-03-19 22:05:07 -0700

    layer style picker added

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvZzM3MWZpbmFsIiwiYSI6ImNqMGcwMHJ1MjAxejcyc
 // initialize map
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v9?optimize=true',
+    style: 'mapbox://styles/geog371final/cj0hn72n6004q2rlb184nvdqy?optimize=true',
     zoom: 3.5,
     center: [-98, 38],
     minZoom: 3,
@@ -20,24 +20,24 @@ map.on('load', function() {
     // get vector tileset sources
     map.addSource('states', {
         'type': 'vector',
-        'url': 'mapbox://geog371final.9l3t21g4'
+        'url': 'mapbox://geog371final.5tkn5wya'
     });
     
     map.addSource('counties', {
         'type': 'vector',
-        'url': 'mapbox://geog371final.9uzwwa8d'
+        'url': 'mapbox://geog371final.atjzbk97'
     });
     
     // add state layer to map
     map.addLayer({
         'id': 'states-layer',
         'source': 'states',
-        'source-layer': 'statesData-4nmjj3',
+        'source-layer': 'statesDataF-6rqrzf',
         'maxzoom': zoomThreshold,
         'type': 'fill',
         'paint': {
             'fill-color': '#4286f4',
-            'fill-opacity': 0.5,
+            'fill-opacity': 0.6,
             'fill-outline-color': '#eee'
         }
     }, 'barrier_line-land-line');
@@ -46,13 +46,62 @@ map.on('load', function() {
     map.addLayer({
         'id': 'counties-layer',
         'source': 'counties',
-        'source-layer': 'countiesData-8g2wfv',
+        'source-layer': 'countiesDataF-8gyqxg',
         'minzoom': zoomThreshold,
         'type': 'fill',
         'paint': {
             'fill-color': '#f47d42',
-            'fill-opacity': 0.5,
+            'fill-opacity': 0.6,
             'fill-outline-color': '#eee'
         }
     }, 'barrier_line-land-line')
 });
+
+// return layer color based on selection
+function setLayerFillColor(lyr, query) {
+    if (query == 'blueOrange') {
+        if (lyr == 'states') {
+            return '#4286f4';
+        }
+        if (lyr == 'counties') {
+            return '#f47d42';
+        }
+    }
+    if (query == 'orangeBlue') {
+        if (lyr == 'states') {
+            return '#f47d42';
+        }
+        if (lyr == 'counties') {
+            return '#4286f4';
+        }
+    }
+}
+
+// get layer color selection
+var lyrSelect = document.getElementById('layerSelect');
+
+// set layer color when there is a new selection
+lyrSelect.addEventListener("click", function() {
+   var lyrChoice = lyrSelect.value;
+    console.log(lyrChoice);
+    
+    map.setPaintProperty('counties-layer', 'fill-color', setLayerFillColor('counties', lyrChoice));
+    map.setPaintProperty('states-layer', 'fill-color', setLayerFillColor('states', lyrChoice));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
